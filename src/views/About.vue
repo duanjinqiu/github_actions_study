9a67fb66dd2411cafa2a4e2b9d92518a2638a1de
duanjinqiu <471568720@qq.com>
2020-01-01 17:29:21 +0800

    git提交并且发布

---
+++ src/views/About.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <p class="red">{{ text }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "About",
+  data() {
+    return {
+      text: "我是好人"
+    };
+  },
+  created() {}
+};
+</script>
+
+<style lang="scss" scoped>
+    .red {
+        color: red;
+    }
+</style>
